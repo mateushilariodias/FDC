781b4608642cdc5681833d3de0523bfcf7b2118a
mateushilariodias <mateusilariodias@gmail.com>
2023-07-22 12:02:26 -0300

    change files and install svgr

---
+++ components/icons.tsx
@@ -0,0 +1,73 @@
+import LogoOneFDC from "../public/images/common/logoOneFDC.svg"
+import LogoTwoFDC from "../public/images/common/logoTwoFDC.svg"
+import Hamb from "../public/images/imagesOfTheHomePage/hamb.svg"
+import FdcOneWaterMark from "../public/images/imagesOfTheHomePage/fdcOneWaterMark.svg"
+import FdcTwoWaterMark from "../public/images/imagesOfTheHomePage/fdcTwoWaterMark.svg"
+import FdcThreeWaterMark from "../public/images/imagesOfTheHomePage/fdcThreeWaterMark.svg"
+import Messagechat  from "../public/images/imagesOfTheHomePage/messagechat.svg"
+import Linkedin from "../public/images/imagesOfTheHomePage/linkedin.svg"
+import Instagram from "../public/images/imagesOfTheHomePage/instagram.svg"
+import Youtube from "../public/images/imagesOfTheHomePage/youtube.svg"
+import Facebook from "../public/images/imagesOfTheHomePage/facebook.svg"
+import Twitter  from "../public/images/imagesOfTheHomePage/twitter.svg"
+import Spotify from "../public/images/imagesOfTheHomePage/spotify.svg"
+import Arrow from "../public/images/imagesOfTheHomePage/arrow.svg"
+import CardImageOne from "../public/images/imagesOfTheHomePage/cardImageOne.svg"
+import CardImageTwo from "../public/images/imagesOfTheHomePage/cardImageTwo.svg"
+import CardImageThree from "../public/images/imagesOfTheHomePage/cardImageThree.svg"
+import CardImageFour from "../public/images/imagesOfTheHomePage/cardImageFour.svg"
+import CardImageFive from "../public/images/imagesOfTheHomePage/cardImageFive.svg"
+import SpotsOne from "../public/images/imagesOfTheHomePage/spotsOne.svg"
+import UpArrow from "../public/images/imagesOfTheHomePage/upArrow.svg"
+import DownArrow from "../public/images/imagesOfTheHomePage/downArrow.svg"
+import MenuOpenClosed from "../public/images/imagesOfTheHomePage/menuOpenClosed.svg"
+import ArrowSite from "../public/images/imagesOfTheHomePage/arrowSite.svg"
+import QuotationMarks from "../public/images/imagesOfTheHomePage/quotationMarks.svg"
+import IconHeader from "../public/images/common/IconHeader.svg"
+import DownArrowList from "../public/images/imagesOfTheHomePage/DownArrowList.svg"
+import Cut from "../public/images/imagesOfTheHomePage/cut.svg"
+import MainLogo from "../public/images/common/logoMain.svg"
+import ListImageOne from "../public/images/imagesOfTheCadastro/listImageOne.svg"
+import ListImageTwo from "../public/images/imagesOfTheCadastro/listImageTwo.svg"
+import ListImageThree from "../public/images/imagesOfTheCadastro/listImageThree.svg"
+import ListImageFour from "../public/images/imagesOfTheCadastro/listImageFour.svg"
+import ListImageFive from "../public/images/imagesOfTheCadastro/listImageFive.svg"
+import ListImageSix from "../public/images/imagesOfTheCadastro/listImageSix.svg"
+
+export {
+    LogoOneFDC,
+    LogoTwoFDC,
+    Hamb,
+    Arrow,
+    CardImageOne,
+    CardImageTwo,
+    CardImageThree,
+    CardImageFour,
+    CardImageFive,
+    SpotsOne,
+    FdcOneWaterMark,
+    FdcTwoWaterMark,
+    FdcThreeWaterMark,
+    Messagechat,
+    Linkedin,
+    Instagram,
+    Youtube,
+    Facebook,
+    Twitter,
+    Spotify,
+    UpArrow,
+    DownArrow,
+    MenuOpenClosed,
+    ArrowSite,
+    QuotationMarks,
+    IconHeader,
+    DownArrowList,
+    Cut,
+    MainLogo,
+    ListImageOne,
+    ListImageTwo,
+    ListImageThree,
+    ListImageFour,
+    ListImageFive,
+    ListImageSix,
+}
